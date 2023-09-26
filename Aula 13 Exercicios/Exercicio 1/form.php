2099f222df3e3aff94262461fd2693b8312cb503
samuelcosta25 <samu.live@outlook.com>
2023-09-25 21:59:04 -0300

    25.09.23

--- Aula 13 Exercicios/Exercicio 1/form.php
+++ Aula 13 Exercicios/Exercicio 1/form.php
@@ -1,4 +1,11 @@
- <?php
-        $area = $_POST["area"];
-        include 'script.php';
-    ?>
+<?php 
+
+include "script.php";
+
+$tam=$_POST['tamanho'];
+
+echo "Em latas de 18L serão utilizadas".lata18($tam)." latas, para pintar todo o espaço mencionado.";
+echo "Em latas de 3.6L serão utilizadas".lata36($tam)." latas, para pintar todo o espaço mencionado.";
+echo "Na mistura de latas e galões visando o menor custo, serão utilizadas".mistura($tam).", para pintar todo o espaço mencionado.";
+
+?>
